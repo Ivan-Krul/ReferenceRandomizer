7c90a0d8b6b6cb661ef9ad20028b370abce34b52
ivan-krul <kiwiivanya128@gmail.com>
2023-05-14 09:23:04 +0300

    changed script

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 function shuffle() {
-    //var img = document.getElementById("image");
-    //var author = document.getElementById("author");
-    //console.log(img);
-    //console.log(author);
     document.getElementById("image").src = "./image/Signed/@AnnasVirtual 0.jpg";
     console.log(document.getElementById("image").src);
 }
