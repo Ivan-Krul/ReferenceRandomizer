ad31230ddc55c0b97198b487060fd6bcf66b717d
ivan-krul <kiwiivanya128@gmail.com>
2023-05-14 09:52:20 +0300

    made a file loader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 function shuffle() {
     document.getElementById("image").src = "./image/Signed/@AnnasVirtual 0.jpg";
     console.log(document.getElementById("image").src);
+
+    const fileLister = "image/images.txt";
+
+    fetch(fileLister)
+        .then(response => response.blob())
+        .then(blob => {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const fileContent = event.target.result;
+                console.log(fileContent);
+            };
+            reader.onerror = function(event) {
+                console.error("Error reading file:", event.target.error)
+            }
+            reader.readAsText(blob);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+    
 }
