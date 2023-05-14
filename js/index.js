39206ab10d93eaee1db1ec64c5237b2f19e652f2
ivan-krul <kiwiivanya128@gmail.com>
2023-05-14 09:19:42 +0300

    added a lot of images and made button logic

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 function shuffle() {
-    var img = document.getElementById("image");
-    var author = document.getElementById("author");
-    console.log(img);
-    console.log(author);
+    //var img = document.getElementById("image");
+    //var author = document.getElementById("author");
+    //console.log(img);
+    //console.log(author);
+    document.getElementById("image").src = "./image/Signed/@AnnasVirtual 0.jpg";
+    console.log(document.getElementById("image").src);
 }
