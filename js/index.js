e4bf23a629ba77e5c7d0e1dfe2e4f10dad6b395f
ivan-krul <kiwiivanya128@gmail.com>
2023-05-14 15:41:18 +0300

    made author output

--- js/index.js
+++ js/index.js
@@ -14,7 +14,6 @@ function readFromFileURL(fileURL) {
           const reader = new FileReader();
           reader.onload = function (event) {
             const text = event.target.result;
-            console.log(event.target.result);
             resolve(text);
           };
           reader.onerror = function (event) {
@@ -34,19 +33,28 @@ function pushToDocument(lines)
 {
     var rand = getRandomInt(lines.length);
 
-    console.log(lines)
     console.log(lines[rand])
 
     if(lines[rand] != "")
+    {
         document.getElementById("image").src = "./image/Signed/" + lines[rand];
+        if(lines[rand].split(' ')[0].indexOf("u-") == 0 && lines[rand].split(' ')[0].indexOf("u-") != null)
+            document.getElementById("author").innerText = "Author: "+ "u/" + lines[rand].split(' ')[0].split('u-')[1];
+        else
+            document.getElementById("author").innerText = "Author: "+ lines[rand].split(' ')[0];
+    }
     else
+    {
         document.getElementById("image").src = "image/not image.png";
+        document.getElementById("author").innerText = "Author: nothing";
+    }
+
+    
 }
 
 async function shuffle() {
     try {
       const text = await readFromFileURL("https://ivan-krul.github.io/ReferenceRandomizer/image/images.txt");
-      console.log(text);
       const lines = getDividedString(text);
       pushToDocument(lines);
     } catch (error) {
