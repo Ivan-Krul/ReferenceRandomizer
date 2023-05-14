c1f91a64693aaf199101baa08dbac34546494de4
ivan-krul <kiwiivanya128@gmail.com>
2023-05-14 14:42:29 +0300

    fixed empty variable and changed style

--- js/index.js
+++ js/index.js
@@ -6,42 +6,50 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }  
 
-function readFromFile(fileURL) {
-    // do not touch this
-    fetch(fileURL)
-    .then(response => response.blob())
-    .then(blob => {
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            const fileContent = event.target.result;
-            return fileContent;
-        };
-        reader.onerror = function(event) {
-            console.error("Error reading file:", event.target.error)
-        }
-        reader.readAsText(blob);
-    })
-    .catch(error => {
-        console.error("Error:", error);
+function readFromFileURL(fileURL) {
+    return new Promise((resolve, reject) => {
+      fetch(fileURL)
+        .then(response => response.blob())
+        .then(blob => {
+          const reader = new FileReader();
+          reader.onload = function (event) {
+            const text = event.target.result;
+            console.log(event.target.result);
+            resolve(text);
+          };
+          reader.onerror = function (event) {
+            console.error("Error reading file:", event.target.error);
+            reject(event.target.error);
+          };
+          reader.readAsText(blob);
+        })
+        .catch(error => {
+          console.error("Error:", error);
+          reject(error);
+        });
     });
+  }
 
-    return "";
-}
-
-function shuffle() {
-    const fileLister = "image/images.txt";
-    let text = readFromFile(fileLister);
-    
-    var lines = getDividedString(text);
-
-    console.log(lines);
-
+function pushToDocument(lines)
+{
     var rand = getRandomInt(lines.length);
 
-    console.log(lines[rand]);
+    console.log(lines)
+    console.log(lines[rand])
 
     if(lines[rand] != "")
         document.getElementById("image").src = "./image/Signed/" + lines[rand];
     else
         document.getElementById("image").src = "image/not image.png";
 }
+
+async function shuffle() {
+    try {
+      const text = await readFromFileURL("https://ivan-krul.github.io/ReferenceRandomizer/image/images.txt");
+      console.log(text);
+      const lines = getDividedString(text);
+      pushToDocument(lines);
+    } catch (error) {
+      console.error("Error during shuffle:", error);
+    }
+  }
