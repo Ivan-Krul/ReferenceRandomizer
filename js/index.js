a667496b0f33b8ac3c0653fb375e5f95ad0d9855
Kiwii Vanya <kiwiivanya128@gmail.com>
2023-07-10 13:56:52 +0300

    commit again

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
 import { getListFromFile } from "./export/reader.js";
 
+function getDividedString(str) {
+  return str.split('\n');
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function readFromFileURL(fileURL) {
+  return new Promise((resolve, reject) => {
+      fetch(fileURL)
+          .then(response => response.blob())
+          .then(blob => {
+              const reader = new FileReader();
+              reader.onload = function (event) {
+                  const text = event.target.result;
+                  resolve(text);
+              };
+              reader.onerror = function (event) {
+                  console.error("Error reading file:", event.target.error);
+                  reject(event.target.error);
+              };
+              reader.readAsText(blob);
+          })
+          .catch(error => {
+              console.error("Error:", error);
+              reject(error);
+          });
+  });
+}
+
+async function getListFromFile() {
+  try {
+      text = await readFromFileURL("https://ivan-krul.github.io/ReferenceRandomizer/image/images.txt");
+      return getDividedString(text);
+  } catch (error) {
+      console.error("Error during shuffle:", error);
+  }
+}
+
 function pushToDocument(lines) {
   var rand = getRandomInt(lines.length);
 
